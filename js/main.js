2b9e4a2d2aaf66670d85521f3b8e210b16023f00
Ramires <moreira.ramires@gmail.com>
2022-06-21 16:53:09 -0300

    criando elementos via js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 const form = document.querySelector("#novoItem");
 console.log(form)
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    console.log("funcionou o submit")
+    
+    //pegar o nome e a quantidade 
+    event.target.elements
+    const nome = event.target.elements['nome'].value;
+    const quantidade = event.target.elements['quantidade'].value;
+
+    criaElemento(nome,quantidade)
+})
+
+function criaElemento(nome,quantidade){
+
+   const novoItemLista = document.createElement('li');
+            novoItemLista.classList.add('item');
+            
+            const numeroItemLista = document.createElement('strong')
+            numeroItemLista.innerHTML=quantidade;
+            
+            
+            novoItemLista.appendChild(numeroItemLista)
+            novoItemLista.innerHTML+=nome;
+
+    const  lista = document.querySelector('[data-lista]');
+            
+    lista.appendChild(novoItemLista)
+  
+
+}
